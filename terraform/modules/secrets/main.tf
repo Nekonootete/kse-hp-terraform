57b6a371f95895929c9031245f8164de26eb5720
Neko Forever <yegachan7709@gmail.com>
2025-07-31 13:48:18 +0900

    production.keyファイルの作成

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -4,5 +4,5 @@ resource "aws_secretsmanager_secret" "rails_master_key" {
 
 resource "aws_secretsmanager_secret_version" "rails_master_key" {
   secret_id     = aws_secretsmanager_secret.rails_master_key.id
-  secret_string = file("${path.module}/../../envs/stg/config/credentials/production.key")
+  secret_string = file("${path.root}/config/credentials/production.key")
 }
