04dd55c1b0269653689cfbb6015cd87f460b3695
Neko Forever <yegachan7709@gmail.com>
2025-07-31 20:08:55 +0900

    terraform fmt

--- terraform/envs/stg/outputs.tf
+++ terraform/envs/stg/outputs.tf
@@ -1,9 +1,9 @@
-output "aws_region"              { value = var.region }
-output "rds_endpoint"            { value = module.rds.db_endpoint }
-output "db_name"                 { value = var.db_name }
-output "db_username"             { value = var.db_username }
-output "rds_ssm_name"            { value = module.rds.rds_ssm_name }
-output "stor_bucket_id"          { value = module.s3_stor.bucket_id }
-output "env_bucket_id"           { value = module.s3_env.bucket_id }
+output "aws_region" { value = var.region }
+output "rds_endpoint" { value = module.rds.db_endpoint }
+output "db_name" { value = var.db_name }
+output "db_username" { value = var.db_username }
+output "rds_ssm_name" { value = module.rds.rds_ssm_name }
+output "stor_bucket_id" { value = module.s3_stor.bucket_id }
+output "env_bucket_id" { value = module.s3_env.bucket_id }
 output "service_cloud_map_rails" { value = var.service_cloud_map_rails }
-output "private_dns_name"        { value = module.private_dns.private_dns_name }
+output "private_dns_name" { value = module.private_dns.private_dns_name }
