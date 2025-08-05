0bc4a2ad7c0b74175eeb9834e75958ce7dc07d0b
Neko Forever <yegachan7709@gmail.com>
2025-08-05 22:34:35 +0900

    ecs exec用ポリシー追加

--- terraform/modules/task_iam/main.tf
+++ terraform/modules/task_iam/main.tf
@@ -34,6 +34,19 @@ data "aws_iam_policy_document" "app_bucket_read" {
   }
 }
 
+data "aws_iam_policy_document" "ecs_exec_ssm" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel",
+    ]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "exec" {
   name               = "task-exec-${var.project_name}-${var.environment}"
   assume_role_policy = data.aws_iam_policy_document.exec_assume.json
@@ -59,6 +72,11 @@ resource "aws_iam_policy" "app_bucket_read" {
   policy = data.aws_iam_policy_document.app_bucket_read.json
 }
 
+resource "aws_iam_policy" "ecs_exec_ssm" {
+  name   = "ecs_exec_ssm-${var.project_name}-${var.environment}"
+  policy = data.aws_iam_policy_document.ecs_exec_ssm.json
+}
+
 resource "aws_iam_role_policy_attachment" "exec_policy" {
   role       = aws_iam_role.exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
@@ -77,4 +95,9 @@ resource "aws_iam_role_policy_attachment" "exec_env_bucket_attach" {
 resource "aws_iam_role_policy_attachment" "task_app_bucket_attach" {
   role       = aws_iam_role.task.name
   policy_arn = aws_iam_policy.app_bucket_read.arn
+}
+
+resource "aws_iam_role_policy_attachment" "task_ecs_exec_ssm_attach" {
+  role       = aws_iam_role.task.name
+  policy_arn = aws_iam_policy.ecs_exec_ssm.arn
 }
